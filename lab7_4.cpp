7a5c0282c9bd890531c2481982c82d52acbe2b34
Nonlnwza007 <waritmanokaew@gmail.com>
2021-11-30 16:30:04 +0700

    finish

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -8,8 +8,19 @@ int main(){
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
-
-    //Write your code here.
+	double gin;
+    cout << "Please input GPA: ";
+	cin >> gin;
+	int i=0;
+	while(i<10){
+		if(gpa[i] >= gin){
+			cout << id[i] << " " << name[i] << " " << gpa[i] << endl;
+			i++;
+		}
+		else{
+			i++;
+		}
+	}
 		
 	return 0;
 }
